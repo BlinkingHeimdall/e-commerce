3fed5a7dbdb48aaf1ea5a32283b07a31f47add8e
Jason Adams <jmadams41@gmail.com>
2022-06-06 21:39:38 -0600

    small tweaks

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
       'stock',
       'category_id'
     ],
-    include: [
+    includeAs: [
       {
         model: Category,
         attributes: ['id', 'category_name']
@@ -48,7 +48,7 @@ router.get('/:id', (req, res) => {
       'stock',
       'category_id'
     ],
-    include: [
+    includeAs: [
       {
         model: Category,
         attributes: ['id', 'category_name']
